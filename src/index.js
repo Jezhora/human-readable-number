818b9c3e51a839285d2ea2c85d7587ba681da614
jezhora <jezhora.photo@gmail.com>
2023-01-16 11:06:03 +0300

    feat: edit func

--- src/index.js
+++ src/index.js
@@ -3,20 +3,17 @@ module.exports = function toReadable (number) {
         tillTwenty: ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
         tens: ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
     }
-
-    if (number === 0){
-        return 'zero';
-    }
-    else if (number < 20) {
-         return numbers.tillTwenty[number.toString()].trim()
-    } else if (number < 100){
-        return `${numbers.tens[number.toString()[0]]} ${numbers.tillTwenty[number.toString()[1]]}`.trim();
-    } else if (number > 99 && number < 1000 && (number % 100)< 10) {
-        return `${numbers.tillTwenty[number.toString()[0]]} hundred ${numbers.tillTwenty[number % 10]}`.trim()
-    } else if (number > 99 && number < 1000 && (number % 100)< 20) {
-        return `${numbers.tillTwenty[number.toString()[0]]} hundred ${numbers.tillTwenty[number % 20]}`
-    } else if (number > 99 && number < 1000) {
-        return `${numbers.tillTwenty[number.toString()[0]]} hundred ${numbers.tens[number.toString()[1]]} ${numbers.tillTwenty[number.toString()[2]]}`.trim()
-    }
+    if (number === 0) {
+         return 'zero';
+    }   else if (number < 20) {
+        return numbers.tillTwenty[number % 20]
+    } else if (number < 100) {
+        return `${numbers.tens[number.toString()[0]]} ${numbers.tillTwenty[number % 10]}`.trim();
+    } else if ((number % 100) < 20) { 
+        // 118 % 100 = 18<20; 105 % 100 = 5<20
+        return `${numbers.tillTwenty[number.toString()[0]]} hundred ${numbers.tillTwenty[number % 100]}`.trim();
+    } else if (number < 1000) {
+        return `${numbers.tillTwenty[number.toString()[0]]} hundred ${numbers.tens[number.toString()[1]]} ${numbers.tillTwenty[number % 10]}`.trim();
+    } 
 
 }
